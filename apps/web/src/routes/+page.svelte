4c6d7ff623c7b6c3b887ec8574d7964451a4d01a
Osman Nuri Okumuş <onokumus@gmail.com>
2025-02-18 13:48:01 +0300

    return pnpm

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -5,3 +5,9 @@
 <h1>Web Site</h1>
 <MyCounterButton />
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+	h1 {
+		color: rebeccapurple;
+	}
+</style>
