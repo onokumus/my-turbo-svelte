f41f47314128ef80fb6c7f1e8023185f546ce90f
Osman Nuri Okumuş <onokumus@gmail.com>
2025-02-18 12:36:25 +0300

    fix:web

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -2,6 +2,6 @@
 	import { MyCounterButton } from '@repo/ui';
 </script>
 
-<h1>Web</h1>
+<h1>Web Site</h1>
 <MyCounterButton />
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
